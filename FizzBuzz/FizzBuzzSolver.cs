875fc3b4a6df6bf16b809d5294d5ef75c3cdf2a0
Jonas Boesch <jonas.boesch@phonak.com>
2014-01-22 19:28:07 +0100

    list instead of dict

--- FizzBuzz/FizzBuzzSolver.cs
+++ FizzBuzz/FizzBuzzSolver.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace FizzBuzz
 {
-    public class FizzBuzzSolver : FizzBuzz.IFizzBuzzSolver
+    public class FizzBuzzSolver : IFizzBuzzSolver
     {
-        private IDictionary<int, string> _strings = new Dictionary<int, string> {{1 , "Fizz"}, {2 , "Buzz"}, {3 , "FizzBuzz"}}; 
+        private readonly IList<string> _strings = new[] {null, "Fizz", "Buzz", "FizzBuzz"};
+
         public string Solve(int numberToPrint)
         {
-            int x = 0;
+            int key = 0;
             if (numberToPrint%3 == 0)
-                x |= 1;
+                key |= 1;
             if (numberToPrint%5 == 0)
-                x |= 2;
+                key |= 2;
 
-            string s;
-            return _strings.TryGetValue(x, out s) ? s : numberToPrint.ToString();
+            string s = _strings[key];
+            return s != null ? s : numberToPrint.ToString();
         }
     }
 }
