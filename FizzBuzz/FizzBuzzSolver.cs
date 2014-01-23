ce8d06a160ad38dcd1a361ea44f21e0e471bb1c1
Jonas Boesch <jonas.boesch@phonak.com>
2014-01-23 10:11:13 +0100

    no list access when returning number.

--- FizzBuzz/FizzBuzzSolver.cs
+++ FizzBuzz/FizzBuzzSolver.cs
@@ -9,13 +9,14 @@ namespace FizzBuzz
         public string Solve(int numberToPrint)
         {
             int key = 0;
+
             if (numberToPrint%3 == 0)
                 key |= 1;
+
             if (numberToPrint%5 == 0)
                 key |= 2;
 
-            string s = _strings[key];
-            return s != null ? s : numberToPrint.ToString();
+            return key == 0 ? numberToPrint.ToString() : _strings[key];
         }
     }
 }
